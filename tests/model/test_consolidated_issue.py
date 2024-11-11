dadffcca7e64eeb3c4168be38a99a649e58b6f4c
Tobias Mikula <72911271+MobiTikula@users.noreply.github.com>
2024-11-11 11:27:06 +0100

    Test for added new logic.

--- tests/model/test_consolidated_issue.py
+++ tests/model/test_consolidated_issue.py
@@ -45,3 +45,84 @@ def test_generate_page_filename_with_none_title(mocker):
         1,
         exc_info=True,
     )
+
+
+# generate_directory_path
+
+
+def test_generate_directory_path_structured_output_disabled_grouping_by_topics_disabled(mocker):
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_output_directory", return_value="/base/output/path")
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_is_structured_output_enabled", return_value=False)
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_is_grouping_by_topics_enabled", return_value=False)
+    mock_issue = Issue(None, None, {"number": 1, "title": "Issue Title"}, completed=True)
+    consolidated_issue = ConsolidatedIssue("organization/repository", mock_issue)
+
+    actual = consolidated_issue.generate_directory_path("issue table")
+
+    assert ["/base/output/path"] == actual
+
+
+def test_generate_directory_path_structured_output_enabled_grouping_by_topics_disabled(mocker):
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_output_directory", return_value="/base/output/path")
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_is_structured_output_enabled", return_value=True)
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_is_grouping_by_topics_enabled", return_value=False)
+    mock_issue = Issue(None, None, {"number": 1, "title": "Issue Title"}, completed=True)
+    consolidated_issue = ConsolidatedIssue("organization/repository", mock_issue)
+
+    actual = consolidated_issue.generate_directory_path("issue table")
+
+    assert ["/base/output/path/organization/repository"] == actual
+
+
+def test_generate_directory_path_structured_output_disabled_grouping_by_topics_enabled_two_issue_topics(mocker):
+    mock_log_debug = mocker.patch("living_documentation_generator.model.consolidated_issue.logger.debug")
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_output_directory", return_value="/base/output/path")
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_is_structured_output_enabled", return_value=False)
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_is_grouping_by_topics_enabled", return_value=True)
+    mock_issue = Issue(None, None, {"number": 1, "title": "Issue Title"}, completed=True)
+    consolidated_issue = ConsolidatedIssue("organization/repository", mock_issue)
+
+    actual = consolidated_issue.generate_directory_path("| Labels | feature, BETopic, FETopic |")
+
+    assert ["/base/output/path/BETopic", "/base/output/path/FETopic"] == actual
+    mock_log_debug.assert_called_once_with(
+                    "Multiple Topic labels found for Issue #%s: %s (%s): %s",
+                    1,
+                    "Issue Title",
+                    "organization/repository",
+                    "BETopic, FETopic",
+                )
+
+
+def test_generate_directory_path_structured_output_disabled_grouping_by_topics_enabled_no_issue_topics(mocker):
+    mock_log_debug = mocker.patch("living_documentation_generator.model.consolidated_issue.logger.debug")
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_output_directory", return_value="/base/output/path")
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_is_structured_output_enabled", return_value=False)
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_is_grouping_by_topics_enabled", return_value=True)
+    mock_issue = Issue(None, None, {"number": 1, "title": "Issue Title"}, completed=True)
+    consolidated_issue = ConsolidatedIssue("organization/repository", mock_issue)
+
+    actual = consolidated_issue.generate_directory_path("| Labels | feature, bug |")
+
+    assert ["/base/output/path/NoTopic"] == actual
+    mock_log_debug.assert_not_called()
+
+
+def test_generate_directory_path_structured_output_enabled_grouping_by_topics_enabled_one_issue_topic(mocker):
+    mock_log_debug = mocker.patch("living_documentation_generator.model.consolidated_issue.logger.debug")
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_output_directory", return_value="/base/output/path")
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_is_structured_output_enabled", return_value=True)
+    mocker.patch("living_documentation_generator.action_inputs.ActionInputs.get_is_grouping_by_topics_enabled", return_value=True)
+    mock_issue = Issue(None, None, {"number": 1, "title": "Issue Title"}, completed=True)
+    consolidated_issue = ConsolidatedIssue("organization/repository", mock_issue)
+
+    actual = consolidated_issue.generate_directory_path("| Labels | feature, BETopic, FETopic |")
+
+    assert ["/base/output/path/organization/repository/BETopic", "/base/output/path/organization/repository/FETopic"] == actual
+    mock_log_debug.assert_called_once_with(
+        "Multiple Topic labels found for Issue #%s: %s (%s): %s",
+        1,
+        "Issue Title",
+        "organization/repository",
+        "BETopic, FETopic",
+    )
