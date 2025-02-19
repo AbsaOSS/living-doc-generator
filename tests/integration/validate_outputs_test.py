9f0f30f1203a1cf07c6d501d61dfea4f969332f7
OlivieFranklova <frankoli@fit.cvut.cz>
2025-02-19 13:56:38 +0100

    Update workflow

--- tests/integration/validate_outputs_test.py
+++ tests/integration/validate_outputs_test.py
@@ -1,6 +1,19 @@
 import os
 
+def count_files_in_directory(directory):
+    return len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
 
-def test_output_folder_is_created():
-    output_folder = "output"
+output_folder = "output"
+directory_path = f"{output_folder}/liv-doc-regime"
+
+def test_validate_for_test1():
     assert os.path.exists(output_folder)
+    assert count_files_in_directory(directory_path) == 4
+
+def test_validate_for_test2():
+    assert os.path.exists(output_folder)
+    assert count_files_in_directory(directory_path) == 2
+
+def test_validate_for_test3():
+    assert os.path.exists(output_folder)
+    assert count_files_in_directory(directory_path) == 2
