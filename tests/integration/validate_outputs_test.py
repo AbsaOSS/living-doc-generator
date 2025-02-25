7408db40231786642271c52e6943533989346b84
OlivieFranklova <frankoli@fit.cvut.cz>
2025-02-25 16:16:25 +0100

    Update tests

--- tests/integration/validate_outputs_test.py
+++ tests/integration/validate_outputs_test.py
@@ -2,10 +2,10 @@ import os
 
 output_folder = "output"
 directory_path = f"{output_folder}/liv-doc-regime"
-issue88_path = f"{directory_path}/88_test_task_issue.md"
-issue89_path = f"{directory_path}/89_test_bug_issue_in_project.md"
-issue90_path = f"{directory_path}/90_test_task_issue_in_project.md"
-issue91_path = f"{directory_path}/91_test_bug_issue.md"
+issue88_path = f"{directory_path}/88_test_documented_feature_.md"
+issue89_path = f"{directory_path}/89_test_user_story_in_project.md"
+issue90_path = f"{directory_path}/90_test_documented_feature_in_project.md"
+issue91_path = f"{directory_path}/91_test_user_story.md"
 
 issue88 = '''
 | Attribute | Content |
@@ -13,80 +13,158 @@ issue88 = '''
 | Organization name | AbsaOSS |
 | Repository name | living-doc-generator |
 | Issue number | 88 |
-| Title | TEST: task issue |
+| Title | TEST: Documented feature  |
 | State | open |
 | Issue URL | <a href='https://github.com/AbsaOSS/living-doc-generator/issues/88' target='_blank'>GitHub link</a>  |
 | Created at | 2025-02-19 08:45:06+00:00 |
-| Updated at | 2025-02-19 08:48:22+00:00 |
+| Updated at | 2025-02-25 13:54:16+00:00 |
 | Closed at | None |
-| Labels | enhancement, int-tests |
+| Labels | int-tests, DocumentedFeature |
 
  
 <h3>Issue Content</h3>
 
-This is a task issue created for integration testing 
+# Feature
+
+This is a documented feature created for integration testing 
 Please **don't remove** this 
-Please **don't add this task to the project** this 
+Please **don't add this task to the project** 
+
+## Description
+
+Feature description
+
+- [ ] subtask 1
+- [ ] subtask 2
 '''
 issue89 = '''
+
 | Attribute | Content |
 |---|---|
 | Organization name | AbsaOSS |
 | Repository name | living-doc-generator |
 | Issue number | 89 |
-| Title | TEST: bug issue in project |
+| Title | TEST: User story in project |
 | State | open |
 | Issue URL | <a href='https://github.com/AbsaOSS/living-doc-generator/issues/89' target='_blank'>GitHub link</a>  |
 | Created at | 2025-02-19 08:48:59+00:00 |
-| Updated at | 2025-02-19 08:58:56+00:00 |
+| Updated at | 2025-02-25 13:47:55+00:00 |
 | Closed at | None |
-| Labels | bug, int-tests |
+| Labels | int-tests, DocumentedUserStory |
 
  
 <h3>Issue Content</h3>
 
-This is a bug issue created for integration testing
-Please **don't remove** this
-Please **don't remove this task from  the project** this
+# User Story
+
+> This is a user story created for integration testing
+> Please **don't remove** this
+> Please **don't remove this task from  the project** 
+
+## Description
+
+Some user story description
+
+## Acceptance Criteria
+
+- [ ] [Criterion 1]
+- [ ] [Criterion 2]
+- [ ] [Criterion 3]
+
+## User Guide
+
+### Description
+
+[Provide a summary of the user story, describing how it supports usability and efficiency.]
+
+### End-to-End Test to Guide Steps
+
+- **Automated**: [True/False]
+- **Prerequisite**:
+    - [List any prerequisites, e.g., At least one searchable object in the database]
+
+- **Steps**:
+    - Step 1: [Instruction]
+    - Step 2: [Instruction]
 '''
-issue90 = '''| Attribute | Content |
+issue90 = '''
+| Attribute | Content |
 |---|---|
 | Organization name | AbsaOSS |
 | Repository name | living-doc-generator |
 | Issue number | 90 |
-| Title | TEST: task issue in project |
+| Title | TEST: Documented feature  in project |
 | State | open |
 | Issue URL | <a href='https://github.com/AbsaOSS/living-doc-generator/issues/90' target='_blank'>GitHub link</a>  |
 | Created at | 2025-02-19 08:55:41+00:00 |
-| Updated at | 2025-02-19 08:55:41+00:00 |
+| Updated at | 2025-02-25 13:54:07+00:00 |
 | Closed at | None |
-| Labels | enhancement, int-tests |
+| Labels | int-tests, DocumentedFeature |
 
  
 <h3>Issue Content</h3>
 
-This is a task issue created for integration testing 
+# Feature
+
+This is a documented feature created for integration testing 
 Please **don't remove** this 
-Please **don't remove this task from the project** this '''
-issue91 = '''| Attribute | Content |
+Please **don't remove this task from the project** 
+
+## Description
+
+Feature description
+
+- [ ] subtask 1
+- [ ] subtask 2
+'''
+issue91 = '''
+| Attribute | Content |
 |---|---|
 | Organization name | AbsaOSS |
 | Repository name | living-doc-generator |
 | Issue number | 91 |
-| Title | TEST: bug issue |
+| Title | TEST: User story |
 | State | open |
 | Issue URL | <a href='https://github.com/AbsaOSS/living-doc-generator/issues/91' target='_blank'>GitHub link</a>  |
 | Created at | 2025-02-19 08:58:08+00:00 |
-| Updated at | 2025-02-19 08:58:08+00:00 |
+| Updated at | 2025-02-25 13:45:46+00:00 |
 | Closed at | None |
-| Labels | bug, int-tests |
+| Labels | int-tests, DocumentedUserStory |
 
  
 <h3>Issue Content</h3>
 
-This is a bug issue created for integration testing
-Please **don't remove** this
-Please **don't add this to the project** this'''
+# User Story
+
+> This is a user story created for integration testing
+> Please **don't remove** this
+> Please **don't add this to the project** 
+
+## Description
+
+Some user story description
+
+## Acceptance Criteria
+
+- [ ] [Criterion 1]
+- [ ] [Criterion 2]
+- [ ] [Criterion 3]
+
+## User Guide
+
+### Description
+
+[Provide a summary of the user story, describing how it supports usability and efficiency.]
+
+### End-to-End Test to Guide Steps
+
+- **Automated**: [True/False]
+- **Prerequisite**:
+    - [List any prerequisites, e.g., At least one searchable object in the database]
+
+- **Steps**:
+    - Step 1: [Instruction]
+    - Step 2: [Instruction]'''
 
 def count_files_in_directory(directory):
     return len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
@@ -97,7 +175,7 @@ def validate_issue(path, issue):
         return issue in markdown_string
 
 
-def test_validate_for_test1():
+def test_validate_for_test_without_project_mining():
     assert os.path.exists(output_folder)
     assert count_files_in_directory(directory_path) == 4 + 1
     assert validate_issue(issue88_path, issue88)
@@ -105,10 +183,16 @@ def test_validate_for_test1():
     assert validate_issue(issue90_path, issue90)
     assert validate_issue(issue91_path, issue91)
 
-def test_validate_for_test2():
+def test_validate_for_test_wit_project_mining():
     assert os.path.exists(output_folder)
-    assert count_files_in_directory(directory_path) == 2 + 1
+    assert count_files_in_directory(directory_path) == 4 + 1
+    assert not validate_issue(issue88_path, issue88)
+    assert not validate_issue(issue89_path, issue89)
+    assert not validate_issue(issue90_path, issue90)
+    assert not validate_issue(issue91_path, issue91)
 
-def test_validate_for_test3():
+def test_validate_for_test_wit_project_mining_and_excluded_project():
     assert os.path.exists(output_folder)
     assert count_files_in_directory(directory_path) == 2 + 1
+    assert not validate_issue(issue88_path, issue88)
+    assert not validate_issue(issue91_path, issue91)
