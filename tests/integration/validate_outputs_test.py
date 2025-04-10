f79df6f8b3618c061abf318a9474a89c838a152e
miroslavpojer <miroslav.pojer@absa.africa>
2025-04-10 10:00:51 +0200

    - Debug of integration tests.

--- tests/integration/validate_outputs_test.py
+++ tests/integration/validate_outputs_test.py
@@ -165,8 +165,37 @@ issue91_content_header_wpmep = '''
 <a href='https://github.com/absa-group/aqueduct-living-documentation/issues/5' target='_blank'>GitHub icon</a>
 '''
 issue91_content = '''
-TODO
-'''
+# User Story
+
+> This is a user story created for integration testing
+> Please **don't remove** this
+> Please **don't add this to the project** 
+
+## Description
+
+Some user story description
+
+## Acceptance Criteria
+
+- [ ] [Criterion 1]
+- [ ] [Criterion 2]
+- [ ] [Criterion 3]
+
+## User Guide
+
+### Description
+
+[Provide a summary of the user story, describing how it supports usability and efficiency.]
+
+### End-to-End Test to Guide Steps
+
+- **Automated**: [True/False]
+- **Prerequisite**:
+    - [List any prerequisites, e.g., At least one searchable object in the database]
+
+- **Steps**:
+    - Step 1: [Instruction]
+    - Step 2: [Instruction]'''
 
 def count_files_in_directory(directory):
     return sum(len(files) for _, _, files in os.walk(directory))
@@ -200,7 +229,7 @@ def test_validate_for_test_with_project_mining():
 
 def test_validate_for_test_with_project_mining_and_excluded_project():
     assert os.path.exists(output_folder)
-    assert count_files_in_directory(directory_path) == 4 + 1
+    assert count_files_in_directory(directory_path) == 6
     assert validate_issue(issue88_path, issue88_header + issue88_content_header_wpmep + issue88_content)
     assert validate_issue(issue89_path, issue89_header + issue89_content_header_wpmep + issue89_content)
     assert validate_issue(issue90_path, issue90_header + issue90_content_header_wpmep + issue90_content)
