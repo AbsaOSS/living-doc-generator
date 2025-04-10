90402f570efb8fed85857c6f5aa1946ce27a42d1
miroslavpojer <miroslav.pojer@absa.africa>
2025-04-10 09:46:24 +0200

    - Debug of integration tests.

--- tests/integration/validate_outputs_test.py
+++ tests/integration/validate_outputs_test.py
@@ -38,7 +38,8 @@ date: 2025-04-10
 weight: 1
 ---
 '''
-issue89_content ='''![GitHub State:Open](https://img.shields.io/badge/GitHub_State-Open-brightgreen)
+issue89_content ='''
+![GitHub State:Open](https://img.shields.io/badge/GitHub_State-Open-brightgreen)
 ![Project State:Open](https://img.shields.io/badge/Project_State-Open-brightgreen)
 ![Priority:Low](https://img.shields.io/badge/Priority-Low-blue)
 <a href='https://github.com/absa-group/aqueduct-living-documentation/issues/5' target='_blank'>GitHub icon</a>
